a11ab6897fdc16eb741c1ae4c3711fded8f5bdfb
fries <jjlauesen@gmail.com>
2025-08-22 16:36:00 -0400

    changed padding. added more template on landing. routing implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { StoriesComponent } from './components/stories/stories.component';
+import { AdoptComponent } from './components/adopt/adopt.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent, pathMatch: 'full' } // default path
+  { path: '', component: LandingComponent, pathMatch: 'full' }, // default path
+  { path: 'about', component: AboutComponent, pathMatch: 'full' }, // about page path
+  { path: 'contact', component: ContactComponent, pathMatch: 'full' }, // contact path
+  { path: 'stories', component: StoriesComponent, pathMatch: 'full' }, // stories path
+  { path: 'adopt', component: AdoptComponent, pathMatch: 'full' } // stories path
 ];
---
+++ src/app/components/adopt/adopt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adopt',
+  imports: [],
+  templateUrl: './adopt.component.html',
+  styleUrl: './adopt.component.css'
+})
+export class AdoptComponent {
+
+}
