7f5d258c51cf8bed4088b7bf40d1050c8319cff8
fries <jjlauesen@gmail.com>
2025-08-29 14:53:51 -0400

    working adopt service calls

--- src/app/components/adopt/adopt.component.ts
+++ src/app/components/adopt/adopt.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AdoptService } from '../../services/adopt.service';
+import { Pet } from '../../models/pet';
 
 @Component({
   selector: 'app-adopt',
@@ -9,4 +11,17 @@ import { RouterLink } from '@angular/router';
 })
 export class AdoptComponent {
 
+
+  private adoptService: AdoptService = inject(AdoptService);
+  private adoptablePets: Pet[] = [];
+
+  consoleLogPets() {
+    console.log(this.adoptablePets);
+  }
+
+  ngOnInit(){
+    this.adoptService.getAdoptablePets().subscribe((pets) => {
+      this.adoptablePets = pets;
+    });
+  }
 }
--- src/app/services/adopt.service.ts
+++ src/app/services/adopt.service.ts
@@ -1,16 +1,20 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
-
+import { environment } from '../../environments/environment';
+import { Pet } from '../models/pet';
 @Injectable({
   providedIn: 'root'
 })
 export class AdoptService {
   private readonly http: HttpClient = inject(HttpClient);
+  private url: string = environment.server_address;
+  private endpoint: string = 'pets/availability';
+  public adoptablePets: Pet[] = [];
 
   public getAdoptablePets(): Observable<Pet[]> {
-    return this.http.get<Pet[]>("url/go/here");
+    const params =  { available: "available" };
+    return this.http.get<Pet[]>(this.url+this.endpoint, { params });
   }
 
 }
